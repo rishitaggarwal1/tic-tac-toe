528cfcb9e1bcb1f1798834a74f7734c13fceabe6
rishitaggarwal1 <rishitaggarwal1@gmail.com>
2020-05-04 20:29:01 +0530

    First Commit Setting the Framework

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Game(models.Model):
+    first_player = models.ForeignKey(
+        User, related_name="first_player", on_delete=models.DO_NOTHING)
+    second_player = models.ForeignKey(
+        User, related_name="second_player", on_delete=models.DO_NOTHING)
+    start_time = models.DateTimeField(auto_now_add=True)
+    last_active = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, default='F')
+
+
+def __str__(self):
+    return "{0} vs {1}".format(
+        self.first_player, self.second_player
+    )
+
+
+class Move(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    comment = models.CharField(max_length=300, blank=True)
+    by_first_player = models.BooleanField()
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
